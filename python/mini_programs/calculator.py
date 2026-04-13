dcae69a428c6241136b081df107e4db287a9f0a4
abrierley0 <brierley.ajb@gmail.com>
2026-04-13 16:19:14 +0100

    Calculator added

--- python/mini_programs/calculator.py
+++ python/mini_programs/calculator.py
@@ -6,3 +6,23 @@ y = int(input("y: "))
 # Truncation no longer an issue
 z = x / y
 print(z)
+
+# 'format' string
+print(f"{z:.50f}")
+
+# Truncation solved, floating point precision problem remains
+
+def get_int(prompt):
+    return int(input(prompt))
+
+
+def main():
+    x = get_int("x: ")
+    y = get_int("y: ")
+
+    print(x + y)
+
+
+main()
+
+# Pythonic to add more lines between functions in Python
