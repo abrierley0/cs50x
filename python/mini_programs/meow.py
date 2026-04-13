f882861d24828cbfaa30252abc5d6fbc625a80a5
abrierley0 <brierley.ajb@gmail.com>
2026-04-13 16:09:41 +0100

    Added meow

---
+++ python/mini_programs/meow.py
@@ -0,0 +1,15 @@
+# Says meow
+
+# Demonstrate functions in Python
+def main():
+    meow(7)
+
+#____________DEFINE HELPER FUNCTIONS AT BOTTOM OF PROGRAM___________________
+def meow(n):
+    for i in range(n):
+        print("meow")
+
+#___________________________________________________________________________
+
+# CALL MAIN AT THE END
+main()
