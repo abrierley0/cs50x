ddf788817e8e1addcb0f44563c00d9380179058a
abrierley0 <brierley.ajb@gmail.com>
2026-04-13 15:34:06 +0100

    Simplify demos

--- python/agree.py
+++ python/agree.py
@@ -3,21 +3,6 @@
 # Demonstrates methods
 s = input("Do you agree? ").lower()
 
-# Make user input uniform
-# s = s.lower()
-
-# Check user input
-# if s == "Y" or s == "y":
-#     print("Agreed.")
-# elif s == "N" or s == "n":
-#     print("Not agreed.")
-
-# Check for errors in user input using array
-# if s in ["y", "yes", "Y", "Yes", "YeS", "YEs"]:
-#     print("Agreed")
-# elif s in ["n", "no"]:
-#     print("Not agreed")
-
 if s in ["y", "yes"]:
     print("Agreed.")
 elif s in ["n", "no"]:
--- python/compare.py
+++ python/compare.py
@@ -2,17 +2,6 @@
 
 from cs50 import get_int
 
-# x = get_int("What is x? ")
-# y = get_int("What is y? ")
-
-# Indentation matters in Python
-# if x < y:
-#     print("x is less than y")
-# elif x > y:
-#     print("x is greater than y")
-# else:
-#     print("x equals y")
-
 # Input reads as type string
 s = input("s: ")
 t = input("t: ")
