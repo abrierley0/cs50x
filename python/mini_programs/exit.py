b42bef9ece4230b8d716d04d30401ecadf1dd9be
abrierley0 <brierley.ajb@gmail.com>
2026-04-14 11:16:06 +0100

    Added exit and greet

--- python/mini_programs/exit.py
+++ python/mini_programs/exit.py
@@ -1,8 +1,10 @@
 import sys
 
+# Check for two command-line arguments
 if len(sys.argv) != 2:
     print("Missing command-line argument.")
     sys.exit()
 
+# Print the second command-line argument
 print(f"hello, {sys.argv[1]}")
 sys.exit(0)
--- python/mini_programs/greet.py
+++ python/mini_programs/greet.py
@@ -1,5 +1,6 @@
 from sys import argv
 
+# Check for two command-line arguments
 if len(argv) == 2:  # Command 'python' is ignored
     print(f"hello, {argv[1]}")
 else:
