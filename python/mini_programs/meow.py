00df341744f7db2c81b9d18d6716fff9d29d6bbd
abrierley0 <brierley.ajb@gmail.com>
2026-04-14 10:48:12 +0100

    meow

--- python/mini_programs/meow.py
+++ python/mini_programs/meow.py
@@ -1,15 +1,15 @@
 # Says meow
 
-# Demonstrate functions in Python
+# Put main first for readability
 def main():
     meow(7)
 
-#____________PUT MAIN AT TOP OF SCRIPT FOR READABILITY___________________
+
+# Define helper functions below
 def meow(n):
     for i in range(n):
         print("meow")
 
-#___________________________________________________________________________
 
 # CALL MAIN AT THE END IS CONVENTIONAL IN PYTHON
 main()
