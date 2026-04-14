a762f762f77631cd0e4944fd5592b89275736175
abrierley0 <brierley.ajb@gmail.com>
2026-04-14 10:36:11 +0100

    Dictionaries in Python

---
+++ python/mini_programs/python_dict.py
@@ -0,0 +1,18 @@
+# Dictionary in Python
+
+# Global list of dictionaries
+people = [ 
+    {"name": "Carter", "number": "+1-617-495-1000"},
+    {"name": "David", "number": "+1-618-495-1000"},
+    {"name": "John", "number": "+1-6949-468-2750"},
+]
+
+name = input("Name: ")
+
+for person in people:
+    if person["name"] == name:
+        number = person["number"]
+        print(f"Found {number}.")
+        break
+else:
+    print("Not found.")
