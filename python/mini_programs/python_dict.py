6d915f24f54eb1e6fe75215fdff0d24a92b4be4d
abrierley0 <brierley.ajb@gmail.com>
2026-04-14 11:08:05 +0100

    Using dicts in Python

--- python/mini_programs/python_dict.py
+++ python/mini_programs/python_dict.py
@@ -1,14 +1,16 @@
-# Dictionaries in Python
+# Searches a list of contacts and returns a phone number
 
 # Global list of dictionaries
 people = [ 
-    {"name": "Carter", "number": "+1-617-495-1000"},
+    {"name": "Carter", "number": "+1-617-495-1000"},    # Each entry like a data struct in C
     {"name": "David", "number": "+1-618-495-1000"},
     {"name": "John", "number": "+1-6949-468-2750"},
 ]
 
 name = input("Name: ")
 
+
+# Search list of names and return corresponding number
 for person in people:
     if person["name"] == name:
         number = person["number"]
@@ -16,3 +18,12 @@ for person in people:
         break
 else:
     print("Not found.")
+
+
+# More Pythonic
+for person in people:
+    if person["name"] == name:
+        print(f"Found {person['number']}")  # Put anything in {}, but use ''
+        break 
+else:
+    print("Not found")
---
+++ python/mini_programs/python_dict_2.py
@@ -0,0 +1,18 @@
+#  Search one big dictionary
+
+# One big dictionary, rather than a list of dictionaries: no name or number keys
+people = {
+    "Carter": "+1-617-495-1000",
+    "David": "+1-618-495-1000",
+    "John": "+1-6949-468-2750",
+}
+
+name = input("Name: ")
+
+if name in people:
+    number = people[name]
+    print(f"Found {number}")
+else:
+    print("Not found.")
+
+
