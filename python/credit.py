c0fea4a4ebecd019c964915c6fad972aa93ccb26
abrierley0 <brierley.ajb@gmail.com>
2026-04-15 13:40:34 +0100

    credit added

--- python/credit.py
+++ python/credit.py
@@ -1,4 +1,4 @@
-# Checks the validity of any credit card number and identifies its type
+# Checks credit card number
 
 MASTERCARD = 16
 AMEX = 15
@@ -9,34 +9,20 @@ def main():
     # Get a credit card number from the user
     number = input("Number: ")
 
-    # Check Mastercard then check sum
-    if check_mastercard(number) == True:
-        if checksum(number, MASTERCARD) == True:
-            print("MASTERCARD.")
-        else:
-            print("INVALID.")
-            return
+    if check_mastercard(number) and checksum(number, MASTERCARD):
+        print("MASTERCARD.")
 
-    # Check Amex then check sum
-    elif check_amex(number) == True:
-        if checksum(number, AMEX) == True:
-            print("AMEX.")
-        else:
-            print("INVALID.")
-            return
+    elif check_amex(number) and checksum(number, AMEX):
+        print("AMEX.")
 
-    # Check Visa then check sum
-    elif check_visa(number) == True:
-        if checksum(number, VISA_13) == True:
-            print("VISA.")
-        elif checksum(number, VISA_16) == True:
-            print("VISA.")
-        else:
-            print("INVALID.")
-            return
+    elif check_visa(number) and checksum(number, VISA_13):
+        print("VISA.")
+
+    elif check_visa(number) and checksum(number, VISA_16):
+        print("VISA.")
+
     else:
-        print("INVALID.")
-        return
+        print("INVALID")
 
 
 def checksum(num, length):
@@ -66,21 +52,24 @@ def checksum(num, length):
 
 
 def check_mastercard(num):
-    """16-digit, begins 51-55"""
+    """Checks if number matches Mastercard - i.e. 16-digit and begins 51-55"""
     if (len(num) == 16) and (int(num[0]) == 5) and (1 <= int(num[1]) <= 5):
         return True
+    return False
 
 
 def check_amex(num):
-    """15-digit, begins 34 or 37"""
+    """Checks if number matches Amex- i.e. 15-digit and begins 34 or 37"""
     if (len(num) == 15) and (int(num[0]) == 3) and (int(num[1]) == 4 or int(num[1]) == 7):
         return True
+    return False
 
 
 def check_visa(num):
-    """13 and 16-digit, begin with 4"""
+    """Checks if number matches Visa - i.e. 13- or 16-digit and begins with 4"""
     if (len(num) == 13 or len(num) == 16) and (int(num[0]) == 4):
         return True
+    return False
 
 
 
